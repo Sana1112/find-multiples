d3f1cefd6980992dcaf14fae281b79e58e94bea7
Sana1112 <94683698+Sana1112@users.noreply.github.com>
2026-02-20 19:55:43 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+ function generateMultiples(){ 
+     let multiples = document.getElementById("multiples"); 
+     multiples.innerHTML = ""; 
+
+     if (this.value === "" || isNaN(this.value)){ 
+         multiples.innerHTML += 'Enter a valid number.'; 
+         return; 
+     } 
+
+     const base = parseInt(this.value); 
+     const nums = [1,2,3,4,5,6,7,8,9,10,11,12]; 
+
+    for(let i =0; i < nums.length; i++){ 
+    const paragraph = document.createElement('p'); 
+    paragraph.innerHTML = base*nums[i]; 
+
+    if (i % 2 === 0){ 
+        paragraph.classList.add('a'); 
+    } else { 
+        paragraph.classList.add('b'); 
+    }
+    multiples.appendChild(paragraph); 
+    } 
+} 
+
+window.addEventListener('load', function(){ 
+    document.getElementById("userInput").addEventListener("keyup", generateMultiples); 
+}); 
+
